b676b6d3f3c3332ac4e285e5a344edf7a9f651d8
noah <noah@cntt.co.kr>
2021-07-01 00:05:25 +0900

    그리디 3번

--- venv/programmers/06.Greedy/greedy_03.py
+++ venv/programmers/06.Greedy/greedy_03.py
@@ -7,7 +7,16 @@ def solution(number, k):
     idx = 0
 
     while k>0:
-        tmpIdx = number[idx: idx+k+1].index(max(number[idx: idx+k+1]))
+        tmpIdx = 0
+        maxNum = int(number[idx])
+        #tmpIdx = number[idx: idx+k+1].index(max(number[idx: idx+k+1]))
+        for i in range(k+1):
+            if int(number[idx+i]) > maxNum:
+                maxNum = int(number[idx+i])
+                tmpIdx = i
+            if maxNum == 9:
+                break
+
         answer += number[idx+tmpIdx]
         idx += tmpIdx+1
         k -= tmpIdx
